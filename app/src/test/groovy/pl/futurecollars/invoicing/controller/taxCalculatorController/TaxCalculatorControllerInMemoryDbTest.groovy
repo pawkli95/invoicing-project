c30116d78761c91838a558538ab6c5f3637f2486
pablo <pawkli95@gmail.com>
2021-11-02 12:58:28 +0100

    First commit of version2

--- app/src/test/groovy/pl/futurecollars/invoicing/controller/taxCalculatorController/TaxCalculatorControllerInMemoryDbTest.groovy
+++
@@ -1,7 +0,0 @@
-package pl.futurecollars.invoicing.controller.taxCalculatorController
-
-import org.springframework.test.context.ActiveProfiles
-
-@ActiveProfiles("inmemory")
-class TaxCalculatorControllerInMemoryDbTest extends TaxCalculatorControllerTest {
-}
