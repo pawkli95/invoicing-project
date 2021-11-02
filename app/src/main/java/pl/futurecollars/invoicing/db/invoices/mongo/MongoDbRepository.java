277b5bbe04a6faa70bf3f1229e6bfd4196d23ea2
pablo <pawkli95@gmail.com>
2021-11-02 12:58:28 +0100

    First commit of version2

--- app/src/main/java/pl/futurecollars/invoicing/db/invoices/mongo/MongoDbRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.futurecollars.invoicing.db.invoices.mongo;
-
-import java.util.UUID;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import pl.futurecollars.invoicing.model.Invoice;
-
-public interface MongoDbRepository extends MongoRepository<Invoice, UUID> {
-}
