79ebe90f08f8cd2230d341aed46607f23ee1fada
pawelklimek <pawkli95@gmail.com>
2021-11-24 16:13:45 +0100

    Code cleanup

--- app/src/main/java/pl/futurecollars/invoicing/service/CompanyService.java
+++ app/src/main/java/pl/futurecollars/invoicing/service/CompanyService.java
@@ -5,8 +5,11 @@ import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import pl.futurecollars.invoicing.db.Database;
+import pl.futurecollars.invoicing.db.companies.CompanyRepository;
 import pl.futurecollars.invoicing.dto.CompanyDto;
 import pl.futurecollars.invoicing.dto.mappers.CompanyMapper;
 import pl.futurecollars.invoicing.exceptions.ConstraintException;
@@ -16,33 +19,39 @@ import pl.futurecollars.invoicing.model.Company;
 @RequiredArgsConstructor
 public class CompanyService {
 
-    private final Database<Company> companyDatabase;
+    private final CompanyRepository companyRepository;
 
     private final CompanyMapper companyMapper;
 
     public CompanyDto saveCompany(CompanyDto companyDto) {
+        if(companyRepository.existsByTaxIdentificationNumber(companyDto.getTaxIdentificationNumber())) {
+            throw new ConstraintException("This tax identification number is already in use");
+        }
         Company company = companyMapper.toEntity(companyDto);
-        Company returnedCompany = companyDatabase.save(company);
-        return companyMapper.toDto(returnedCompany);
+        return companyMapper.toDto(companyRepository.save(company));
     }
 
     public CompanyDto getById(UUID id) {
-        Company company = companyDatabase.getById(id);
+        Company company = companyRepository.getById(id);
         return companyMapper.toDto(company);
     }
 
     public void delete(UUID id) throws NoSuchElementException, ConstraintException {
-        companyDatabase.delete(id);
+        try {
+            companyRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new ConstraintException("To delete company, delete related invoices first");
+        }
     }
 
     public List<CompanyDto> getAll() {
-        return companyDatabase.getAll().stream()
+        return companyRepository.findAll().stream()
                 .map(companyMapper::toDto).collect(Collectors.toList());
     }
 
     public CompanyDto update(CompanyDto companyDto) {
         Company company = companyMapper.toEntity(companyDto);
-        Company returnedCompany = companyDatabase.update(company);
+        Company returnedCompany = companyRepository.save(company);
         return companyMapper.toDto(returnedCompany);
     }
 }
